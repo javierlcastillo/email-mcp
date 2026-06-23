efe15e9b5c1e99fb219a454ee9f970ccf3acdd6d
javierlcastillo <jazer2228@gmail.com>
2026-06-22 23:07:00 -0600

    Switched from server.py to app.py as the root directory as well as render fixes

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import uvicorn
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from src.mcp.tools import mcp, health
 from src.middleware.middleware import TokenAuthMiddleware
 
 if __name__ == "__main__":
     app = mcp.sse_app()
     app.add_middleware(TokenAuthMiddleware)
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
     app.add_route("/health", health, methods=["GET"])
     uvicorn.run(
         app,
